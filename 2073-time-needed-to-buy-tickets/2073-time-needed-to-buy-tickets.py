a5abedf6aa2618d9013fca0b94996b6f55db36f1
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-05-28 16:52:35 +0300

    Update 2073-time-needed-to-buy-tickets.py

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -43,15 +43,6 @@ class Solution:
         
         
         
-        # time = 0
-        # while tickets[k] > 0:
-        #     for i in range(len(tickets)):
-        #         if tickets[i] > 0:
-        #             tickets[i] -=1
-        #             time +=1
-        #         if tickets[k] == 0:
-        #             break
-        # return time 
-                
+        
             
         
