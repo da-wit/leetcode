c7eafc055473f2f204cf691e3873b3884e1948c6
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-11-12 14:44:55 +0300

    Time: 63 ms (5.07%), Space: 17 MB (19.16%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,21 +1,30 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        # nums.sort()
-        def isGreat (n,m):
-            if str(n) + str(m) > str(m) +str(n):
-                return True
-        if 0 in set(nums):
-            if len(set(nums)) == 1:
-                return "0"
+        
+        nums.sort()
+        def isit(l,r):
+            print(l,r)
+            x = l+r
+            y= r+l
+            return x < y
+        x = set(nums)
+        if len(x)==1 and 0 in nums:
+            return "0"
+    
         for i in range(len(nums)):
             left = i
-            right = len(nums) -1
+            right = len(nums)-1
+            print(nums)
             while left < right:
-                if not isGreat(nums[left],nums[right]):
-                    nums[left],nums[right] = nums[right],nums[left]
-                right -=1
-        return "".join(map(str,nums))
+                if not isit(str(nums[left]),str(nums[right])):
+                    right -=1
+                else:
+                    nums[left],nums[right]=nums[right],nums[left]
+                    right -=1
                     
+        return "".join(map(str,nums))
+    
+                
                 
 
 
