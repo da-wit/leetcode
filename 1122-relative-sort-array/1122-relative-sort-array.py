d52f3d7a1d45a450c9a9d7c029689171282e83cb
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-11-12 09:24:20 +0300

    Time: 55 ms (5.61%), Space: 17 MB (16.54%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,18 +1,24 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        new = []
-        arr1.sort()
+        left =0 
         for i in range(len(arr2)):
-            right = 0
+            right = left +1
             while right < len(arr1):
-                if arr1[right] == arr2[i]:
-                    new.append(arr1[right])
+                if arr2[i] == arr1[left]:
+                    left +=1
                     right +=1
-                else:
+                elif arr2[i] != arr1[left] and arr2[i] != arr1[right]:
                     right +=1
-        for i in arr1:
-            if i not in arr2:
-                new.append(i)
+                elif (arr2[i] != arr1[left] and arr2[i] == arr1[right]):
+                    arr1[left],arr1[right]=arr1[right],arr1[left]
+                    left +=1
+                    right +=1
+                elif(arr2[i] == arr1[left] and arr2[i] == arr1[right]):
+                    left +=1
+            print(arr1)
+        arr1[left:]=sorted(arr1[left:])
+                
 
-        return new
-        
+            
+        return(arr1)
+
