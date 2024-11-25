8fb0ff5e132309b23a8fd06e83537db51142ade6
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-11-25 12:56:50 +0300

    Time: 72 ms (14.44%), Space: 16.8 MB (35.6%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,79 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        m = 0
-        x = defaultdict(int)
+        d =defaultdict(int)
+        
         left = 0
-        for right in range(len(s)):
-            x[s[right]] +=1
-            
-               
-            
+        m = 0
 
-            while x[s[right]] > 1:
-                
-                x[s[left]] -=1
-                if x[s[left]] == 0:
-                    del x[s[left]]
+        for i in range(len(s)):
+            
+            d[s[i]] +=1 
+            while d[s[i]] >1:
+                d[s[left]] -=1
+                if d[s[left]] == 0:
+                    del[d[s[left]]]
                 left +=1
-            
-            m = max(m,right - left + 1)
+            m = max(m,i - left +1)
         return m
-                
-
-
-
-
-
-
-
-
-
-
-        # x = list(map(str,s))
-        # y = []
-        # z,m= 0,0
-        # i =0
-        # while i < len(x):
-        #     if x[i] not in y:
-        #         y.append(x[i])
-        #         z+=1
-        #         m = max(m,z)
-        #         i+=1
-        #     else:
-        #         y.pop(0)
-        #         z-=1
-        # return m
-                
-
-    
-            
-
-
-        # x = {}
-        # left = 0
-        # max_substring = 0
-        # if s  == " ":
-        #     return 1
-
-        # for right in range(len(s)): 
-        #     if s[right] not in x:
-        #         x[s[right]] = 1
-        #     else:
-        #         x[s[right]] += 1
-        #     while x[s[right]] >= 2:
-        #         max_substring = max(max_substring,right-left)
-        #         x[s[left]] -= 1
-        #         left += 1
-
-        # return max_substring
-        # d = {}
-        # left = 0
-        # for i in range(len(s)):
-        #     if s[i] not in x:
-        #         d[s[i]] = 1
-        #     else:
-        #         d[s[i]] +=1
-
-        #     while d[s[i]] > 1:
-                
-
