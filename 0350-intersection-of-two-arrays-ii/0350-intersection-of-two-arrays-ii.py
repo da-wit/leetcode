9c24aa85ccb6f22b18d3b3d013a5aef51adec8a0
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-11-28 09:13:23 +0300

    Time: 0 ms (100%), Space: 17.6 MB (5.09%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -5,23 +5,44 @@ class Solution:
         #     if (i in nums2):
         #         result.append(i)
         #         nums2.remove(i)
-        l = 0
-        r = 0
-        while l<len(nums1) and r < len(nums2):
-            if(len(nums1)>=len(nums2)):
-                if (nums1[l] == nums2[r]):
-                    result.append(nums1[l])
-                    l+=1
-                    r+=1
-                else:
-                    l+=1
-            else:
-                if (nums1[l] == nums2[r]):
-                    result.append(nums1[l])
-                    l+=1
-                    r+=1
-                else:
-                    r+=1
+        # a =defaultdict(int)
+        # b=defaultdict(int)
+        # for i in nums1:
+        #     a[i]+=1
+        # for j in nums2:
+        #     b[j]+=1
+        # for k,v in a.items():
+        #     if k in b:
+        #         res = [k] * min(a[k],b[k])
+        #         result +=res
+        # return result
+                    
 
+        nums1.sort()
+        nums2.sort()
+        if len(nums1)>=len(nums2) :
+            l =0 
+            r =0
+            while r < len(nums1) and l < len(nums2):
+                if nums1[r] == nums2[l]:
+                    result.append(nums2[l])
+                    l+=1
+                    r+=1
+                elif nums1[r] > nums2[l]:
+                    l+=1
+                else:
+                    r+=1
+        else:
+            l=0
+            r =0
+            while r < len(nums2) and l < len(nums1):
+                if nums2[r]==nums1[l]:
+                    result.append(nums1[l])
+                    l+=1
+                    r+=1
+                elif nums2[r] > nums1[l]:
+                    l+=1
+                else:
+                    r+=1
 
         return result
