0e28b192b03b19adba0c085e910a2b73294b9c47
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-12-01 16:17:36 +0300

    Time: 170 ms (19.31%), Space: 58 MB (94.16%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,84 +1,26 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        answer = []
+        win = defaultdict(int)
+        loss = defaultdict(int)
+        for i in range(len(matches)):
+            win[matches[i][0]]+=1
+            loss[matches[i][1]]+=1
+        res=[]
         winner =[]
-        losser =[]
-        dloss = defaultdict(int)
-        dwin = defaultdict(int)
-        for i in matches:
-            # print(i[1]) 
-            if i[1] in dloss:
-                dloss[i[1]] +=1
+        lost_once= []
+        for key,val in win.items():
+            if key not in loss:
+                winner.append(key)
             else:
-                dloss[i[1]] =1
-        for i in matches:
-            if i[0] not in dloss:
-                if i[0] not in winner:
-                    winner.append(i[0])
-                
-
+                if loss[key]-1 == 0:
+                    lost_once.append(key)
+                    del loss[key]
+                else:
+                    del loss[key]
+        for k ,v in loss.items():
+            if v == 1:
+                lost_once.append(k)
+        res.append(sorted(winner))
+        res.append(sorted(lost_once))
+        return res
         
-        # print(dloss)
-        for key,val in dloss.items():
-            if val == 1:
-                losser.append(key)
-        
-            
-
-
-        return [sorted(winner),sorted(losser)]
-        # left =0
-        
-
-        # for l in range(len(matches)):
-        #     # right = 0
-        #     count =0
-        #     c =0
-        #     for r in range(l,len(matches)):
-        #         if r != l:
-        #             print(matches[l][0] , matches[r][1])
-        #             if matches[l][0] == matches[r][1]:
-        #                 count +=1
-        #             if matches[l][1] == matches[r][1]:
-        #                 c +=1
-
-        #     if count == 1:
-        #         if matches[l][0] not in losser:
-        #             losser.append(matches[l][0])
-        #     if count == 0:
-        #         if matches[l][0] not in winner:
-        #             winner.append(matches[l][0])
-        #     if c == 0:
-        #         if matches[l][1] not in losser:
-        #             losser.append(matches[l][1])
-           
-            # while right < len(matches):
-                 
-            #     if left != right:
-            #         if matches[left][0] == matches[right][1]:
-            #             count +=1
-                
-            #         if matches[left][1] == matches[right][1]:
-            #             c +=1
-                
-        
-            #     right += 1
-       
-
-            # if count == 1 :
-            #     if matches[left][0] not  in losser:
-            #         losser.append(matches[left][0])
-            # if count == 0:
-            #     if matches[left][0] not in winner:
-            #         winner.append(matches[left][0])
-            # if c == 1:
-            #     if matches[left][1] not in losser:
-            #         losser.append(matches[left][1])
-                    
-
-
-
-
-
-
-            
