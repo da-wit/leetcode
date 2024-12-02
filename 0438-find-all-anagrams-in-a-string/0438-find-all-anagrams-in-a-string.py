04474c8de9003fb0c7e1efc10b89f2c4f4917b93
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-12-02 10:32:08 +0300

    Time: 46 ms (62.31%), Space: 18 MB (6.67%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,25 +1,29 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        x = []
-        k = len(p)
-        pcount = Counter(p)
-        scount = Counter(s[:k])
-
-        for i in range(len(s) - k):
-            if scount == pcount:
-                x.append(i)
+        w = len(p)
+        res = []
+        if len(p) > len(s):
+            return []
+        dp = defaultdict(int)
+        ds= defaultdict(int)
+        for i in p:
+            dp[i]+=1
+        for i in s[:w]:
+            ds[i]+=1
+        l =0
+        if ds ==dp:
+            res.append(l)
+        for r in range(w,len(s)):
+           
+            ds[s[l]]-=1
+            if ds[s[l]] == 0:
+                del ds[s[l]]
+            ds[s[r]]+=1
+            l+=1
+            if ds ==dp:
+                res.append(l)
+        # if (ds==dp):
+        #     res.append(len(s)-w)
+        return res
             
-            scount[s[i]] -=1
 
-            if scount[s[i]] == 0:
-                del scount[s[i]]
-            scount[s[i + k]] += 1
-
-        if scount == pcount:
-            x.append(len(s) - k)
-        return x
-            
-
-
-
-        
