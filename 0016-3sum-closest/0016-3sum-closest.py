891709ceafe1e62427733f141e736d7bd84a93ff
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-12-05 16:07:06 +0300

    Time: 609 ms (6.68%), Space: 18 MB (7.05%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -2,25 +2,28 @@ class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
         x = {}
-        m = float('inf')
-        for i in range(len(nums)):
+        
+        for i in range(len(nums)-2):
             left = i + 1
             right = len(nums)-1
             while left < right:
                 s =nums[i] + nums[left] + nums[right] 
+                
+                x[s] = abs(s - target)
                 if s > target:
-                    x[s] = s - target
-                else:
-                    x[s] = target - s
-
-                left +=1
-                right -=1
-                while nums[left] == nums[left -1] and left < right:
-                    left +=1
-                while nums[right] == nums[right + 1] and right >left :
                     right -=1
-        print(x)
+                else:
+                    left +=1
+               
+                # left +=1
+                # right -=1
+                # while nums[left] == nums[left -1] and left < right:
+                #     left +=1
+                # while nums[right] == nums[right + 1] and right >left :
+                #     right -=1
+      
         y = min(x.values())
+        print(x.items())
         for key, val in x.items():
             if val == y:
                 return key
