8a3f11bbf48f918701cd1353538f882294e57e0f
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-12-21 17:43:18 +0300

    Time: 73 ms (40.35%), Space: 21.3 MB (30.14%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -4,16 +4,14 @@ class Solution:
         l =0
         c =0
         for r in range(len(nums)):
-            if nums[r] !=1:
+            if nums[r] == 0:
                 flip -=1
             while flip < 0:
-                if nums[l] == 1:
+                if nums[l] == 0:
                     flip +=1
-                
                 l+=1
-
-            c = max(c,r-l+1)
-        return c-1
+            c = max(c,r-l)
+        return c
 
 
                 
