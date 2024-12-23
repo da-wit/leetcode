8c72396ebf206386ff3589a71c2d1b1b6a542dce
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-12-23 19:43:41 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,33 +1,61 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
-        left = 0
-        right = len(people)-1
         count = 0
-        print(people)
-        while left <= right:
-            if limit == people[right]:
-                count +=1
-                right -=1
-            elif people[left] + people[right] <= limit :
-                count +=1
-                left +=1
-                right -=1
-            else:
-                count+=1
-                right -=1
-        return (count)
+        left = 0
+
+        # return count  
+        # print(people)
+        # while left < len(people)-1:
+        #     right = left +1
+        #     print(left,right) 
+        #     print(people[left],people[right])
+            
+        #     if limit - people[left] == people[right]:
+        #         count +=1
+        #         left +=2
                 
-
-
+        #     elif limit - people[left] != people[right]  and people[right] == people[-1]:
+        #         count +=1
+        #         left +=1  
+            
         
+           
+        # return count 
+
+        print(people)
+        while left<len(people):
+            right = left + 1
             
 
-
-
-
-             
-
-
-
-
+            print(count,left,right)
+            if people[left] == limit:
+                count +=1
+                left +=1
+            elif people[right]== limit:
+                count +=1
+                left +=1 
+            elif  limit - people[left] == people[right]:
+                count +=1
+                left +=2
+                # right +=2
+                print("first")
+            elif  limit - people[left] != people[right]:
+                count +=1
+                left +=1
+                # right +=2
+                print("second")
+            else:
+                count+=1
+                left +=1
+            # elif  limit - people[left] !=people[right]:
+            #     count +=1
+            #     left +=1
+            #     # right +=1
+            #     print("second")
+            # elif people[left] - limit !=people[right] and people[right] == people[len(people)-1]:
+            #     count +=2
+            #     left +=1
+            #     # right +=1
+            #     print("thired")
+        return count
