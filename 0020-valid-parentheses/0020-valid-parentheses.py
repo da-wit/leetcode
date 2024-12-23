f745433f18c12905375a7a5d158cab6b34b8b624
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-12-24 00:13:33 +0300

    Time: 4 ms (9.89%), Space: 17.6 MB (13.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,42 +2,14 @@ class Solution:
     def isValid(self, s: str) -> bool:
         d = {"(":")","{":"}","[":"]"}
         stack =[]
-        val = d.keys()
-        for i in s:
-            if i in val:
-                stack.append(i)
+        for i in range(len(s)):
+            if s[i] in d:
+                stack.append(s[i])
             else:
                 if not stack:
                     return False
-                x= stack.pop()
-                if d[x] != i:
+                elif d[stack[-1]] != s[i]:
                     return False
-        return not stack
-
-
-
-
-
-
-
-
-
-
-
-
-        # stack = []
-        # y = d.keys()
-        # print(y)
-        # for i in range(len(s)):
-           
-        #     if s[i] in y:
-        #         stack.append(s[i])
-        #     else:
-        #         if not stack:
-        #             return False
-        #         x = stack.pop()
-        #         if d[x] != s[i]:
-        #             print(stack)
-        #             return False
-        # return stack == []
-
+                else:
+                    stack.pop()
+        return stack == []
